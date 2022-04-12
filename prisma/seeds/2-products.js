9a2e3ee87d4750ff3129dc5203ec5aedf000ba57
jamesprenticez <jamesprenticez@gmail.com>
2022-04-12 20:59:01 +1200

    Hooking Product API up to DB

--- prisma/seeds/2-products.js
+++ prisma/seeds/2-products.js
@@ -3,7 +3,7 @@ const products = [
     id: 'ckzywzytm00413svi6jezdk1o',
     name: 'Yellow Regular Fit Oxford Cotton Shirt',
     slug: 'yellow-regular-fit-oxford-cotton-shirt',
-    catagory: 'Shirts',
+    category: 'Shirts',
     image: '/images/shirts/shirt1/1.avif',
     brand: 'Lacrosse ',
     description: 'Made in compact Oxford cotton authentic and durable, this shirt softens and becomes softer with each wash. This solid-coloured shirt has it all: a classic button-down collar, a patch pocket on the chest, and real mother-of-pearl buttons. Its available in several contemporary pastel colours and will look great with a V-neck sweater and raw rolled-up jeans.',
