ad7bbe34789db0d1c28b768b3a2f7cee49863d39
jamesprenticez <jamesprenticez@gmail.com>
2022-04-15 11:34:19 +1200

    Delete item from cart!

--- redux/cart/actions.js
+++ redux/cart/actions.js
@@ -1,3 +1,8 @@
-export const setCartItems = (product, color, quantity) => {
-  return ({ type: "SET_CART_ITEMS", payload: {...product, color, quantity} })
+export const setCartItems = (item, color, quantity) => {
+  return ({ type: "SET_CART_ITEMS", payload: {...item, color, quantity} })
 }
+
+export const deleteCartItem = (item) => {
+  return ({ type: "DELETE_CART_ITEM", payload: item})
+}
+
--- redux/cart/reducer.js
+++ redux/cart/reducer.js
@@ -5,7 +5,6 @@ const intialState = {
   ]
 }
 
-
 export default function cart(state=intialState, action){
   switch(action.type) {
     case "SET_CART_ITEMS":
@@ -13,11 +12,15 @@ export default function cart(state=intialState, action){
       const newItem = action.payload
       const itemExists = state.cartItems.find(item => item.id == newItem.id)
 
-      // This removes the need for an update function
-      //If itemExists then overwrite with newItem otherwise concat cart with newItem 
+      // This removes the need for an UPDATE_CART_ITEM case
+      // If itemExists then overwrite with newItem otherwise concat cart with newItem 
       const updatedCartItems = itemExists ? state.cartItems.map(item => item.id == itemExists.id ? newItem : item) : [...state.cartItems, newItem]
-      //console.log(updatedCartItems)
       return {...state, cartItems: updatedCartItems}
+
+    case "DELETE_CART_ITEM":  
+      const itemToDelete = action.payload
+      const filteredCartItems = state.cartItems.filter(item => item.id !== itemToDelete.id)
+      return {...state, cartItems: filteredCartItems}
     
     default: 
       return state
