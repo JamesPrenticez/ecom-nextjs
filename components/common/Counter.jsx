c4f456c5481da458c22509e8d99a40ec96ffcd39
jamesprenticez <jamesprenticez@gmail.com>
2022-04-15 11:02:06 +1200

    Fixing up the styles on Counter *contentEditable

--- components/common/Counter.jsx
+++ components/common/Counter.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
+function Minus({className}){
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M20 12H4" />
+    </svg>
+  )
+}
+
+function Plus({className}){
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+    </svg>
+  )
+}
+
 export default function Counter({item, handleClick, handleChange}) {
   console.log(item)
   const currQty = item.quantity
@@ -8,13 +24,27 @@ export default function Counter({item, handleClick, handleChange}) {
 
   return (
     <div className='flex'>
-      <button disabled={currQty <= min} onClick={() => handleClick(item, currQty - 1)} className="w-12 bg-gray-300 p-2 rounded-tl-md rounded-bl-md disabled:cursor-not-allowed">
-        &ndash;
+      <button 
+        disabled={currQty <= min} onClick={() => handleClick(item, currQty - 1)}
+        className="bg-gray-300 p-1 rounded-tl-md rounded-bl-md disabled:cursor-not-allowed"
+      >
+        <Minus className="h-[1rem] w-[1rem]"/>
       </button>
-      <input type="number" value={currQty} onChange={(e) => handleChange(e, item, min, max)} className="text-center outline-none p-2"/>
-        <button 
-          disabled={currQty >= max} onClick={() => handleClick(item, currQty + 1)} className="w-12 bg-gray-300 p-2 rounded-tr-md rounded-br-md disabled:cursor-not-allowed">
-        &#43;
+      <div 
+        contenteditable="true"
+        type="number"
+        value={currQty || min}
+        onChange={(e) => handleChange(e, item, min, max)}
+        className="flex text-center justify-center outline-none py-1 text-[1rem] w-[3rem]" 
+      >
+        {currQty || min}
+      </div>
+      <button 
+        disabled={currQty >= max} 
+        onClick={() => handleClick(item, currQty + 1)}
+        className="bg-gray-300 p-1 rounded-tr-md rounded-br-md disabled:cursor-not-allowed"
+      >
+        <Plus className="h-[1rem] w-[1rem]"/>
       </button>
   </div> 
   )
