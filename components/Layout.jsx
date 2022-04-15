6d4913272786f281fae8e4bb4d98bc87362edbeb
jamesprenticez <jamesprenticez@gmail.com>
2022-04-15 19:57:04 +1200

    Drop down for signed in user

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import NextLink from 'next/link'
 import CountrySelector from './nav/CountrySelector'
@@ -8,60 +8,91 @@ import NextImage from 'next/image'
 
 export default function Layout({title, description, children}) {
   const {data: session} = useSession();
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
       <Head>
-        <title>{title ? `${title} | ${process.env.NEXT_PUBLIC_COMPANY_NAME}` : process.env.NEXT_PUBLIC_COMPANY_NAME}</title>
-        {description ? <meta name='description' content={description}/> : <meta name="description" content="E-commerce general store" />}
+        <title>
+          {title
+            ? `${title} | ${process.env.NEXT_PUBLIC_COMPANY_NAME}`
+            : process.env.NEXT_PUBLIC_COMPANY_NAME}
+        </title>
+        {description ? (
+          <meta name="description" content={description} />
+        ) : (
+          <meta name="description" content="E-commerce general store" />
+        )}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className="bg-secondary-background">
-        <nav className='flex items-center h-16 w-full max-w-7xl mx-auto pr-4  text-secondary-text'>
-          
+        <nav className="flex items-center h-16 w-full max-w-7xl mx-auto pr-4 text-secondary-text">
           {/* Left */}
           <NextLink href="/" passHref>
-            <a className='text-2xl bold font-bold hover:text-secondary-hover'>
+            <a className="text-2xl bold font-bold hover:text-secondary-hover">
               {process.env.NEXT_PUBLIC_COMPANY_NAME}
             </a>
           </NextLink>
 
           {/* Right */}
-          <div className='ml-auto space-x-6 inline-flex items-center'>
+          <div className="ml-auto space-x-6 inline-flex items-center">
             <CountrySelector />
             <CartButton />
 
-            { !session ? 
-            <a 
-              className='text-lg hover:text-text-secondary-hover flex items-center cursor-pointer transform transition-all hover:scale-110 duration-150 ease-in-out select-none'
-              onClick={signIn}
-            >
-              Login
-            </a>
-            :
-            <>
-              <NextLink href={`/user/${session?.user.email}/settings`} passHref>
-              <a 
-                className='text-lg hover:text-text-secondary-hover cursor-pointer transform transition-all hover:scale-110 duration-150 ease-in-out select-none'>
-                {session?.user.email}
+            {!session ? (
+              <a
+                className="text-lg hover:text-text-secondary-hover flex items-center cursor-pointer transform transition-all hover:scale-110 duration-150 ease-in-out select-none"
+                onClick={signIn}
+              >
+                Login
               </a>
-              </NextLink>
-              <div className="tooltip">
-                <div className='h-[2.5rem] w-[2.5rem] rounded-full object-cover hover:cursor-pointer overflow-hidden bubble'>
+            ) : (
+              <div
+                className="flex whitespace-nowrap items-center space-x-4 relative"
+                onClick={() => setIsOpen(!isOpen)}
+                onMouseLeave={() => setIsOpen(false)}
+              >
+                <p className="text-lg">{session?.user.name}</p>
+
+                <div
+                  className="h-[2.5rem] w-[2.5rem] bg-red-500 rounded-full object-cover hover:cursor-pointer overflow-hidden bubble"
+                  
+                >
                   <NextImage
                     src={session?.user.image}
                     alt={session?.user.image}
-                    onClick={signOut}
                     width={25}
                     height={25}
                     layout={"responsive"}
                   />
                 </div>
-                <span className="tooltiptext">Sign Out</span>
+                {/* Hitbox for onmouseleave */}
+                <div className={`${isOpen ? "block" : "hidden"} bg-transparent w-64 h-64 absolute top-0 -right-[1rem] z-40`}></div>
+                <div
+                  className={`${isOpen ? "block" : "hidden"} absolute z-50 top-[3.8rem] -right-[1rem] w-64 bg-secondary-background cursor-pointer border-2 border-secondary-background rounded-lg tooltiptext`}
+                >
+                  <div
+                    className="p-4 hover:bg-primary-background hover:text-primary-text rounded-tl-md rounded-tr-md border-b"
+                    onClick={() => {}}
+                  >
+                    <p>Profile</p>
+                  </div>
+                  <div
+                    className="p-4 hover:bg-primary-background hover:text-primary-text border-b"
+                    onClick={() => {}}
+                  >
+                    <p>My Account</p>
+                  </div>
+                  <div
+                    className="p-4 hover:bg-primary-background hover:text-primary-text rounded-bl-md rounded-br-md"
+                    onClick={signOut}
+                  >
+                    <p>Sign Out</p>
+                  </div>
+                </div>
               </div>
-            </>
-            }
+            )}
           </div>
         </nav>
       </header>
@@ -71,9 +102,11 @@ export default function Layout({title, description, children}) {
       </main>
 
       <footer className="flex items-center justify-center h-16 bg-secondary-background text-secondary-text">
-        <p>All rights reserved. {process.env.NEXT_PUBLIC_COMPANY_NAME} &trade;</p>
+        <p>
+          All rights reserved. {process.env.NEXT_PUBLIC_COMPANY_NAME} &trade;
+        </p>
       </footer>
     </>
-  )
+  );
 }
 
